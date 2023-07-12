ba1e86230a3142481f6ced970a5100d04f56b9b8
abrahamkunnan <114346602+abrahamkunnan@users.noreply.github.com>
2023-07-12 18:17:43 -0500

    implemented stemming, the chatbot can now familiarize itself with rootwords. Uncomment the print statements if you want to the code in action

--- Chatbot.py
+++ Chatbot.py
@@ -6,15 +6,21 @@ import string
 import warnings
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
 warnings.filterwarnings('ignore')
 filtered_list = []
 
+
+
+f = open('C:\depression.txt','r',errors = 'ignore')
 #make sure you save the google doc as a txt file to your hard drive (idk if the location matters) and name the txt file as "depression" 
 #you may need to change line 9 depending on where you saved this txt file and what you named it
-f = open('C:\depression.txt','r',errors = 'ignore')
 
 raw = f.read()
 raw = raw.lower()
+
 
 #comment out the following lines after running the code once
 #nltk.download('punkt')
@@ -25,7 +31,14 @@ sent_tokens = nltk.sent_tokenize(raw)
 word_tokens = nltk.word_tokenize(raw)
 stop_words = set(stopwords.words("english"))
 
-#uncomment and adjust 45 to a lower or higher number to print more or less text. only for testing purposes
+stemmer = PorterStemmer()
+stemmed_words = [stemmer.stem(word) for word in word_tokens]
+#reduces the words down to their root. Helps the chatbot get familiar with words that come from the same word
+#example, reduced, reducing, and reduction have the root word reduce. Uncomment line 38 to print.
+#print(stemmed_words)
+
+
+#Uncomment and adjust 45 to a lower or higher number to print more or less text. only for testing purposes
 #print(word_tokens[:45])
 
 #stop words are words that can be removed without changing the meaning of the sentence
@@ -35,8 +48,7 @@ for i in word_tokens:
 	if i.casefold() not in stop_words or i.casefold() == "not":
 		filtered_list.append(i)
 
-#printing the is only used to show that the code works
-#this prints out all the text, aside from the stop words
-print(filtered_list)
+#Uncomment this line to print the entire text file without stopwords. only used to show that the code works
+#print(filtered_list)
 
 
